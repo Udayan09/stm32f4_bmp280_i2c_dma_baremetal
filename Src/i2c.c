05a63ddd8cc03f4b9e9e7af565ce8065e8042d8d
FIXED-TERM Borah Udayan (BGSW/PJ-ETA-P) <ruf3kor@bosch.com>
2026-02-24 16:55:15 +0530

    Added i2c1 init

--- Src/i2c.c
+++ Src/i2c.c
@@ -1,7 +1,34 @@
 #include "i2c.h"
+#include "gpio.h"
 
-void i2c_init(I2C_TypeDef *i2c){
-    if (i2c == I2C1)    RCC->APB1ENR |= (1 << 21);
-    else if (i2c == I2C2)    RCC->APB1ENR |= (1 << 22);
-    else if (i2c == I2C3)    RCC->APB1ENR |= (1 << 23);
+
+#define I2C1EN (1U << 21)
+
+
+#define PB6_I2C_SCL_ALT_FUNC	4
+#define PB7_I2C_SDA_ALT_FUNC	4
+
+
+void I2C1_init(){       //PB7->SDA,     PB6->SCL
+
+    //Enable clock access
+    RCC->APB1ENR |= I2C1EN;
+
+    //Enable GPIO port
+    gpio_init(GPIOB);
+
+    //Set the gpio modes
+    gpio_pin_init(GPIOB, 6, GPIO_ALTERNATE, PB6_I2C_SCL_ALT_FUNC);
+	gpio_pin_init(GPIOB, 7, GPIO_ALTERNATE, PB7_I2C_SDA_ALT_FUNC);
+
+    //Set output mode to open drain
+    gpio_set_output_mode(GPIOB, 6, GPIO_OUTPUT_MODE_OPEN_DRAIN);
+    gpio_set_output_mode(GPIOB, 7, GPIO_OUTPUT_MODE_OPEN_DRAIN);
+
+    //Set Gpio pins to pullup
+    gpio_set_pullup_pulldown(GPIOB, 6, GPIO_PULLUP);
+    gpio_set_pullup_pulldown(GPIOB, 7, GPIO_PULLUP);
+ 
+    //Enter i2c reset mode
+    I2C1->CR1 |= (1U << 15);
 }
